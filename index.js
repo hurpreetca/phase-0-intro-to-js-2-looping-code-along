ff1f2bf3822905509fa52e84372af019c0b9db61
Harpreet Singh <hurpreet@icloud.com>
2023-03-11 18:59:43 -0500

    Looping Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+const gifts= ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event)
+{
+    let messages= [];
+    for (let i=0; i< names.length; i++)
+    {
+    messages.push("Thank you, "+names[i]+", for the wonderful surprise gift!")
+    }
+    return messages;
+}
+
+//console.log["Guadalupe", "Ollie", "Aki"];
+//console.log(writeCards(names,));
+
+
+
+function countDown(i)
+{
+    
+    while(i>=0)
+    {
+        console.log(i);
+        i--;
+    }
+ 
+}
+
